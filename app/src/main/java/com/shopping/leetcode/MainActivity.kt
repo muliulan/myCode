32f3741babcbdf3f60fa281a7955e7f0991fa5a7
zhaochenshuo <953655369@qq.com>
2021-02-18 11:31:56 +0800

    回滚

--- app/src/main/java/com/shopping/leetcode/MainActivity.kt
+++ app/src/main/java/com/shopping/leetcode/MainActivity.kt
@@ -16,12 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val currentTimeMillis = System.currentTimeMillis()
-
-        val codeAA = Main11().aa()
-        Log.e("mll_codeAA","$codeAA   ${System.currentTimeMillis()-currentTimeMillis}")
+//        val currentTimeMillis = System.currentTimeMillis()
+//
+//        val codeAA = Main11().aa()
+//        Log.e("mll_codeAA","$codeAA   ${System.currentTimeMillis()-currentTimeMillis}")
 
 //        val offerAA = Offer6().aa()
 //        Log.e("mll_offerAA", "$offerAA   ${System.currentTimeMillis() - currentTimeMillis}")
+
+        val arrayList = ArrayList<String>()
+        val arrayList1:ArrayList<String> ?= null
+        val arrayList2 = ArrayList<String>()
+
+        if (arrayList.size == 0 && arrayList1?.size == 0 && arrayList2.size == 0) {
+            Log.e("mll_offerAA","1111111111111111111")
+        }else{
+            Log.e("mll_offerAA","22222111")
+
+        }
+
     }
 }
